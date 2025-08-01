714daea111b116b457287be799adac7fb29a3e19
Ali Haider Khan <haider.salar@gmail.com>
2025-08-01 16:10:17 +0530

    chore: init git for version control

---
+++ script/leg.gd
@@ -0,0 +1,97 @@
+extends Marker2D
+
+@onready var joint_1: Marker2D = $joint1
+@onready var joint_2: Marker2D = $joint1/joint2
+@onready var hand: Marker2D = $joint1/joint2/hand
+
+const MIN_DISTANCE = 52
+
+var len_upper = 0
+var len_middle = 0
+var len_lower = 0
+
+@export var flipped = true
+
+var goal_pos = Vector2()
+var int_pos = Vector2()
+var start_pos = Vector2()
+var step_height = 18
+var step_rate =  0.3
+var step_time = 0.0
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	len_upper = joint_1.position.x
+	len_middle = joint_2.position.x
+	len_lower = hand.position.x
+	
+	if flipped:
+		$Sprite2D.flip_h = true
+		joint_1.get_node("Sprite2D").flip_h = true
+		joint_2.get_node("Sprite2D").flip_h = true
+		
+
+func _physics_process(delta: float) -> void:
+	step_time += delta
+	var target_pos = Vector2()
+	var t = step_time / step_rate
+	if t < 0.5:
+		target_pos = start_pos.lerp(int_pos, t / 0.5)
+	elif t < 1.0:
+		target_pos = int_pos.lerp(goal_pos, (t - 0.5) / 0.5)
+	else:
+		target_pos = goal_pos
+	update_ik(target_pos)
+
+func step(g_pos):
+	if goal_pos == g_pos:
+		return
+	goal_pos = g_pos
+	var hand_pos = hand.global_position
+	var highest = goal_pos.y
+	if hand_pos.y < highest:
+		highest = hand_pos.y
+	var mid = (goal_pos.x + hand_pos.x) / 2.0
+	start_pos = hand_pos
+	int_pos = Vector2(mid, highest - step_height)
+	step_time = 0.0
+ 
+
+func update_ik(target_pos):
+	var offset = target_pos - global_position
+	var dist_to_target = offset.length()
+	if dist_to_target < MIN_DISTANCE:
+		offset = (offset / dist_to_target) * MIN_DISTANCE
+		dist_to_target = MIN_DISTANCE
+	var base_r = offset.angle()
+	var len_total = len_upper + len_middle + len_lower
+	var len_dummy_side = (len_upper + len_middle) * clamp(dist_to_target / len_total, 0.0, 1.0)
+	
+	var base_angles = SSS_calc(len_dummy_side, len_lower, dist_to_target)
+	var next_angles = SSS_calc(len_upper, len_middle, len_dummy_side)
+	
+	global_rotation = base_angles.B + next_angles.B + base_r
+	joint_1.rotation = next_angles.C
+	joint_2.rotation = base_angles.C + next_angles.A
+
+func SSS_calc(side_a,side_b,side_c):
+	if side_c >= side_a + side_b:
+		return {"A": 0, "B": 0, "C": 0}
+	var angle_a = law_of_cosine(side_b, side_c, side_a)
+	var angle_b = law_of_cosine(side_c, side_a, side_b) + PI
+	var angle_c = PI - angle_a - angle_b
+	
+	if flipped:
+		angle_a = -angle_a
+		angle_b = -angle_b
+		angle_c = -angle_c
+	return {"A": angle_a, "B": angle_b, "C": angle_c}
+	
+	
+
+
+func law_of_cosine(a,b,c):
+	if 2*a*b == 0:
+		return 0
+	return acos((a*a + b*b - c*c)/(2*a*b))
---
+++ script/spider.gd
@@ -0,0 +1,107 @@
+extends CharacterBody2D
+
+@onready var low_mid_check: RayCast2D = $LowMidCheck
+@onready var high_mid_check: RayCast2D = $HighMidCheck
+@onready var front_check: RayCast2D = $FrontCheck
+@onready var back_check: RayCast2D = $BackCheck
+@onready var front_mid_check: RayCast2D = $FrontMidCheck
+@onready var back_mid_check: RayCast2D = $BackMidCheck
+
+@onready var front_leg_node: Node2D = $FrontLegs
+@onready var back_leg_node: Node2D = $BackLegs
+
+var front_legs = []
+var back_legs = []
+
+
+@export var x_speed = 25
+@export var y_speed = 20
+@export var step_rate = 0.3
+
+var time_since_last_step = 0
+var cur_f_leg = 0
+var cur_b_leg = 0
+var use_front = false
+
+@export var base_height := 40
+@export var duck_height := 70.0
+@export var rise_height := 40.0
+@export var smooth_height := 5.0
+
+var target_height := 0.0
+var current_height := 0.0
+var base_y := 0.0
+
+var stay_down := false
+
+func _ready():
+	base_y = global_position.y
+	front_legs = front_leg_node.get_children()
+	back_legs = back_leg_node.get_children()
+	front_check.force_raycast_update()
+	back_check.force_raycast_update()
+	front_mid_check.force_raycast_update()
+	back_mid_check.force_raycast_update()
+	for i in range(8):
+		step()
+
+func _draw() -> void:
+	draw_line(Vector2.ZERO, front_mid_check.target_position, Color.RED)
+	draw_line(Vector2.ZERO, back_mid_check.target_position, Color.BLUE)
+	
+
+func _physics_process(delta):
+	var mouse_pos = get_global_mouse_position()
+	var dir = (mouse_pos - global_position).normalized()
+	var move_vec = dir * x_speed
+
+	# --- Anchoring the body in world space (original logic) ---
+	if high_mid_check.is_colliding():
+		move_vec.y = -y_speed  # move upward to avoid ceiling
+	elif !low_mid_check.is_colliding():
+		move_vec.y = y_speed   # move downward to reach ground
+		
+	if stay_down == true:
+		current_height = lerp(current_height, target_height, delta * smooth_height)
+	# --- Bracing the pose based on what's ahead (new logic) ---
+	var moving_forward = dir.x >= 0.0
+	var obstacle_ahead = front_mid_check.is_colliding() if moving_forward else back_mid_check.is_colliding()
+	var ledge_ahead    = !front_mid_check.is_colliding() if moving_forward else !back_mid_check.is_colliding()
+	if obstacle_ahead:
+		stay_down = true
+		target_height = duck_height
+	elif ledge_ahead:
+		target_height = rise_height
+	else:
+		target_height = base_height
+	current_height     = lerp(current_height, target_height, delta * smooth_height)
+	global_position.y  = base_y + current_height
+	move_and_collide(move_vec * delta)
+
+
+func _process(delta):
+	time_since_last_step += delta
+	if time_since_last_step >= step_rate:
+		time_since_last_step = 0
+		step()
+ 
+
+func step():
+	if front_legs.is_empty() or back_legs.is_empty():
+		print("Leg lists not initialized yet")
+		return
+	var leg = null
+	var sensor = null
+	if use_front:
+		leg = front_legs[cur_f_leg]
+		cur_f_leg += 1
+		cur_f_leg %= front_legs.size()
+		sensor = front_check
+	else:
+		leg = back_legs[cur_b_leg]
+		cur_b_leg += 1
+		cur_b_leg %= back_legs.size()
+		sensor = back_check
+	use_front = !use_front
+	var target = sensor.get_collision_point()
+	leg.step(target)
