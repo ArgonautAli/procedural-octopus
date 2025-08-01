1f2866e5da7308fa5b55bd8740bd9377ab6c8fe1
Ali Haider Khan <haider.salar@gmail.com>
2025-08-01 18:48:55 +0530

    feat: started ducking + rising

--- script/spider.gd
+++ script/spider.gd
@@ -10,6 +10,9 @@ extends CharacterBody2D
 @onready var front_leg_node: Node2D = $FrontLegs
 @onready var back_leg_node: Node2D = $BackLegs
 
+@onready var front_up_check: RayCast2D = $FrontUpCheck
+@onready var body_up_check: RayCast2D = $BodyUpCheck
+
 var front_legs = []
 var back_legs = []
 
@@ -24,7 +27,7 @@ var cur_b_leg = 0
 var use_front = false
 
 @export var base_height := 40
-@export var duck_height := 70.0
+@export var duck_height := 85.0
 @export var rise_height := 40.0
 @export var smooth_height := 5.0
 
@@ -48,6 +51,9 @@ func _ready():
 func _draw() -> void:
 	draw_line(Vector2.ZERO, front_mid_check.target_position, Color.RED)
 	draw_line(Vector2.ZERO, back_mid_check.target_position, Color.BLUE)
+	draw_line(Vector2.ZERO, front_up_check.target_position, Color.BLUE)
+	draw_line(Vector2.ZERO, body_up_check.target_position, Color.RED)
+	
 	
 
 func _physics_process(delta):
@@ -62,16 +68,18 @@ func _physics_process(delta):
 		move_vec.y = y_speed   # move downward to reach ground
 		
 	if stay_down == true:
-		current_height = lerp(current_height, target_height, delta * smooth_height)
+		current_height = lerp(current_height, duck_height, delta * smooth_height)
 	# --- Bracing the pose based on what's ahead (new logic) ---
 	var moving_forward = dir.x >= 0.0
-	var obstacle_ahead = front_mid_check.is_colliding() if moving_forward else back_mid_check.is_colliding()
+	var obstacle_ahead = front_mid_check.is_colliding()  || body_up_check.is_colliding() || front_up_check.is_colliding() if moving_forward else back_mid_check.is_colliding()
 	var ledge_ahead    = !front_mid_check.is_colliding() if moving_forward else !back_mid_check.is_colliding()
 	if obstacle_ahead:
 		stay_down = true
 		target_height = duck_height
+		print("ducking: ", target_height)
 	elif ledge_ahead:
 		target_height = rise_height
+		stay_down = false
 	else:
 		target_height = base_height
 	current_height     = lerp(current_height, target_height, delta * smooth_height)
@@ -105,3 +113,5 @@ func step():
 	use_front = !use_front
 	var target = sensor.get_collision_point()
 	leg.step(target)
+
+# test
